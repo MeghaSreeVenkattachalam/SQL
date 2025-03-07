29243c0528e26a479151c4109e8e61f663bc60c3
MEGHA SREE VENKATTACHALAM <meghavenkattachalam@gmail.com>
2025-03-07 22:23:16 +0530

    Create Fetch Table.sql

---
+++ Fetch Table.sql
@@ -0,0 +1,26 @@
+ Let us output all the data entered in the table "employee"(its created already).
+
+Employee_id	Employee_Name	Department
+1	Kayla Thompson	Sales
+2	Ethan Chen	Operations
+3	Julia Lee	Hr
+4	Marcus Garcia	Product
+5	Samantha Park	Operations
+
+In SQL Select * from Table_Name is used to print table in console.
+
+CODE:
+
+SELECT * from employee;
+
+OUTPUT:
+
+┌─────────────┬────────────────┬────────────┐
+│ Employee_id │ Employee_Name  │ Department │
+├─────────────┼────────────────┼────────────┤
+│ 1           │ Kayla Thompson │ Sales      │
+│ 2           │ Ethan Chen     │ Operations │
+│ 3           │ Julia Lee      │ Hr         │
+│ 4           │ Marcus Garcia  │ Product    │
+│ 5           │ Samantha Park  │ Operations │
+└─────────────┴────────────────┴────────────┘

a97dd38e48bda26bb03bec9c902c5bda392c26f6
MEGHA SREE VENKATTACHALAM <meghavenkattachalam@gmail.com>
2025-03-08 23:24:30 +0530

    Create Create Table.sql

---
+++ Create Table.sql
@@ -0,0 +1,23 @@
+Create table
+
+SYNTAX:
+
+ CREATE TABLE student (
+       Student_id INT,
+       Student_Name  TEXT,
+       Department  TEXT
+    ); 
+
+EXAMPLE:
+Write a query to create a table 'employee', with columns employee_id, employee_Name and Department.
+
+CODE:
+
+CREATE TABLE employee
+(
+    Employee_id INT , /* Integer Data type assigned to the variable */
+    Employee_Name   TEXT, /* TEXT Data type assigned to the variable */
+    Department  TEXT /* TEXT Data type assigned to the variable */
+);
+
+

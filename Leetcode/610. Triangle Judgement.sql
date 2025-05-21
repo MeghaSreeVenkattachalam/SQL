f144175eea71ae8f7e8fe93eb08ad741b6cb44a0
MEGHA SREE VENKATTACHALAM <meghavenkattachalam@gmail.com>
2025-05-21 22:01:05 +0530

    Create 610. Triangle Judgement.sql

---
+++ Leetcode/610. Triangle Judgement.sql
@@ -0,0 +1,54 @@
+Table: Triangle
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| x           | int  |
+| y           | int  |
+| z           | int  |
++-------------+------+
+In SQL, (x, y, z) is the primary key column for this table.
+Each row of this table contains the lengths of three line segments.
+ 
+
+Report for every three line segments whether they can form a triangle.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Triangle table:
++----+----+----+
+| x  | y  | z  |
++----+----+----+
+| 13 | 15 | 30 |
+| 10 | 20 | 15 |
++----+----+----+
+Output: 
++----+----+----+----------+
+| x  | y  | z  | triangle |
++----+----+----+----------+
+| 13 | 15 | 30 | No       |
+| 10 | 20 | 15 | Yes      |
++----+----+----+----------+
+
+CODE:
+
+# Write your MySQL query statement below
+
+SELECT X,Y,Z, CASE WHEN X+Y<=Z || Y+Z<=X || X+Z<=Y THEN 'No'
+ELSE 'Yes'
+END AS triangle FROM Triangle;
+
+OUTPUT:
+
+Runtime
+312
+ms
+Beats
+20.48%

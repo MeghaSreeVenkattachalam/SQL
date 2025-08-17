d8cf7fad0ddbb8820a36c69c85d84e25d2b6f3e0
MEGHA SREE VENKATTACHALAM <meghavenkattachalam@gmail.com>
2025-08-17 13:49:45 +0530

    Update 1661. Average Time of Process per Machine Solved.sql

--- Leetcode/1661. Average Time of Process per Machine Solved.sql
+++ Leetcode/1661. Average Time of Process per Machine Solved.sql
@@ -82,6 +82,7 @@ GROUP BY machine_id, process_id
 ) as t
 GROUP BY machine_id;
 
+
 OUTPUT:
 
 Runtime
@@ -89,4 +90,25 @@ Runtime
 ms
 Beats
 74.55%
+
+
+ANOTHER APPROACH CODE: (USING SELF-JOINS)
+
+ # Write your MySQL query statement below
+
+SELECT a.machine_id,
+ROUND(AVG(b.timestamp-a.timestamp),3) AS processing_time
+FROM Activity a INNER JOIN Activity b ON a.machine_id = b.machine_id 
+AND a.process_id = b.process_id
+AND a.activity_type = 'start'
+AND b.activity_type = 'end'
+GROUP BY machine_id;
+
+OUTPUT:
+
+ Runtime
+248
+ms
+Beats
+51.41
 

e7a890d0cfd4e057ee34c287442b1f9a82c0b01f
MEGHA SREE VENKATTACHALAM <meghavenkattachalam@gmail.com>
2025-08-18 19:27:04 +0530

    Create 1164. Product Price at a Given Date.sql

---
+++ Leetcode/1164. Product Price at a Given Date.sql
@@ -0,0 +1,63 @@
+Table: Products
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| product_id    | int     |
+| new_price     | int     |
+| change_date   | date    |
++---------------+---------+
+(product_id, change_date) is the primary key (combination of columns with unique values) of this table.
+Each row of this table indicates that the price of some product was changed to a new price at some date.
+Initially, all products have price 10.
+
+Write a solution to find the prices of all products on the date 2019-08-16.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Products table:
++------------+-----------+-------------+
+| product_id | new_price | change_date |
++------------+-----------+-------------+
+| 1          | 20        | 2019-08-14  |
+| 2          | 50        | 2019-08-14  |
+| 1          | 30        | 2019-08-15  |
+| 1          | 35        | 2019-08-16  |
+| 2          | 65        | 2019-08-17  |
+| 3          | 20        | 2019-08-18  |
++------------+-----------+-------------+
+Output: 
++------------+-------+
+| product_id | price |
++------------+-------+
+| 2          | 50    |
+| 1          | 35    |
+| 3          | 10    |
++------------+-------+
+ 
+
+
+CODE:
+
+# Write your MySQL query statement below
+SELECT P.product_id, P.new_price As price From Products P JOIN
+( SELECT product_id, MAX(change_date) AS latest_date 
+FROM Products 
+WHERE change_date<='2019-08-16'
+GROUP BY product_id ) AS T
+ON P.product_id = T.product_id AND P.change_date = T.latest_date;
+
+OUTPUT:
+
+Runtime
+1628
+ms
+Beats
+5.00%
